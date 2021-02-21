c31a5970ab7a037c0ea25fa2ff02960a8a501cfc
wackyjoker <kevin.young8931@gmail.com>
2021-02-21 02:09:35 +0100

    first commit

---
+++ src/components/Header/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <header>
+        <nav>
+            <li>{{msg}}</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+        </nav>
+    </header>
+</template>
+
+<script lang="ts">
+    import { Options, Vue } from "vue-class-component";
+
+    @Options({
+        props: {
+            msg: String
+        }
+    })
+    export default class Header extends Vue {
+        msg!: string;
+    }
+    </script>
+
+<style  lang="scss" scoped>
+    @import 'index.scss';
+    </style>
